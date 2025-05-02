fda9304533b11c4230d35514df3743b4dd93486a
MD SIFAT ALI <sifatali520@gmail.com>
2025-05-03 02:27:56 +0600

    up database

--- zoyro/settings.py
+++ zoyro/settings.py
@@ -124,13 +124,13 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
         
-    }
-}
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
