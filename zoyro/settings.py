6c9f8e160c59c2ba1c8ddc91f07cfb1f0ea74567
MD SIFAT ALI <sifatali520@gmail.com>
2025-05-03 01:55:09 +0600

    for dep

--- zoyro/settings.py
+++ zoyro/settings.py
@@ -168,10 +168,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
